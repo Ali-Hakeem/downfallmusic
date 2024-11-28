e731c475121b4707e8ee198a65e486b6b31022cf
Ali-Hakeem <figmapaulo@gmail.com>
2024-11-28 15:05:49 +0700

    Update search.blade.php

--- resources/views/post/search.blade.php
+++ resources/views/post/search.blade.php
@@ -18,7 +18,7 @@
                             <h2 class="card-title h4 fw-bold"><a class="text-decoration-none text-black" href="/{{$item->slug}}">{{$item->title}}</a></h2>
                             <span class="medium text-muted">By {{$item->author}} | {{date('d/m/Y', strtotime($item->created_at))}}</span><hr>
                         <a class="text-decoration-none text-black" href="/{{$item->slug}}">
-                            <div class="p-2 img-fluid" style="margin-bottom: -45px;"><blockquote class="instagram-media" data-instgrm-permalink="{{$item->embed}}" data-instgrm-version="14" 
+                            <div class="img-fluid" style="margin-bottom: -45px;"><blockquote class="instagram-media" data-instgrm-permalink="{{$item->embed}}" data-instgrm-version="14" 
                                 style=" background:#FFF; border:0; border-radius:0; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; padding:0; width:99.375%; width:-webkit-calc(40% - 2px); width:calc(100% - 2px);">
                                 </blockquote>
                                 <script async src="//www.instagram.com/embed.js"></script>
