203ea9ddfef68358d60e319bf48943eab790ee71
Ali-Hakeem <figmapaulo@gmail.com>
2024-11-28 15:24:25 +0700

    Update post.blade.php

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -15,9 +15,9 @@
                     @foreach ($list as $index => $item)
                     <div class="card mb-4 rounded-0">
                         <div class="card-body">
-                            <h2 class="card-title h4 fw-bold"><a class="text-decoration-none text-black" href="/{{$item->slug}}">{{$item->title}}</a></h2>
+                            <h2 class="card-title h4 fw-bold"><a class="text-decoration-none text-black" href="/article/{{$item->slug}}">{{$item->title}}</a></h2>
                             <span class="medium text-muted">By {{$item->author}} | {{date('d/m/Y', strtotime($item->created_at))}}</span><hr>
-                        <a class="text-decoration-none text-black" href="/{{$item->slug}}">
+                        <a class="text-decoration-none text-black" href="/article/{{$item->slug}}">
                             <div class="p-2 img-fluid" style="margin-bottom: -45px;"><blockquote class="instagram-media" data-instgrm-permalink="{{$item->embed}}" data-instgrm-version="14" 
                                 style=" background:#FFF; border:0; border-radius:0; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; padding:0; width:99.375%; width:-webkit-calc(40% - 2px); width:calc(100% - 2px);">
                                 </blockquote>
@@ -28,7 +28,7 @@
                         <div class="card-body">
                             <hr>
                             <p class="card-text">{!! substr($item->body, 0, 120)!!}...</p>
-                            <a class="text-decoration-none text-black" href="/{{$item->slug}}">
+                            <a class="text-decoration-none text-black" href="/article/{{$item->slug}}">
                                 <h5 class="float-end fw-bold">Read more</h5>
                             </a>
                         </div>
